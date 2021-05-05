62bd0b3c664f9362cc09a6523ed62b3d839a5ffe
Ugne Valionyte <ugva@itu.dk>
2021-05-05 16:10:59 +0200

    fix

--- InsertionLinked.js
+++ InsertionLinked.js
@@ -5,7 +5,7 @@ class InsertionLinked extends Phaser.Scene {
     }
 
     preload() {
-        this.load.spritesheet('onion', 'Assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+        // this.load.spritesheet('onion', 'Assets/dude.png', { frameWidth: 32, frameHeight: 48 });
         this.load.image('onion', 'Assets/onion.png');
     }
 
